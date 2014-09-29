8552b1cb8103e758ea96819f01b3deb2794968fb
Mauricio Gamboa <mgamboa@mobiquityinc.com>
2014-09-29 12:32:26 -0600

    EventsGlue Set Up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,61 @@
 module.exports = function(grunt) {
+
   grunt.initConfig({
+
     less: {
       development: {
         options: {
-          compress: true,
-          yuicompress: true,
-          optimization: 2
+          paths: ["public/styles"]
         },
         files: {
-          // target.css file: source.less file
-          "css/main.css": "less/main.less"
+          "public/styles/styles.css": "public/less/styles.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["public/styles"],
+          cleancss: true
+        },
+        files: {
+          "public/styles/styles.css": "public/less/styles.less"
         }
       }
     },
-    watch: {
-      styles: {
-        files: ['less/**/*.less'], // which files to watch
-        tasks: ['less'],
-        options: {
-          nospawn: true
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      js: {
+        src: ['bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'public/js/eventsGlue.js'],
+        dest: 'public/js/main.js',
+      }
+    },
+
+    uglify: {
+      js: {
+        files: {
+          'public/js/main.js': 'public/js/main.js'
         }
+      }
+    },
+
+    watch: {
+      js: {
+        files: [
+        'bower_components/jquery/dist/jquery.js', 'bower_components/bootstrap/dist/js/bootstrap.js', 'public/js/eventsGlue.js', 'public/js/main.js'],
+        tasks: ['concat:js', 'uglify:js']
+      },
+      styles: {
+        files: ['public/less/*.less'],
+        tasks: ['less']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('default', ['watch']);
