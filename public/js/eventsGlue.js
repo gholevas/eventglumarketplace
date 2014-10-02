db81ead79ff4e22d68f9218e0721714addf6c7fa
Mauricio Gamboa <magalocr@gmail.com>
2014-10-01 22:59:29 -0600

    Venues Partial

--- public/js/eventsGlue.js
+++ public/js/eventsGlue.js
@@ -21,6 +21,45 @@ $(function() {
   $('#calendar-2').datepicker();
   $('#calendar-3').datepicker();
   $('#calendar-4').datepicker();
+  $('#calendar-5').datepicker();
+  $('#calendar-aside').datepicker();
+
+  $('.budget-slider').slider({
+    range: true,
+    min: 0,
+    max: 5000,
+    values: [1000, 2000],
+    slide: function( event, ui ) {
+      $(".bg-text").text( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+    }
+  });
+
+  $('.bg-text').text( "$" + $('.budget-slider').slider( "values", 0 ) + " - $" + $('.budget-slider').slider( "values", 1 ) );
+
+  $('.guests-slider-aside').slider({
+    range: "min",
+    min: 1,
+    max: 100,
+    value: 10,
+    slide: function(event, ui) {
+      $('.guests-text').text(ui.value + ' People');
+    }
+  });
+
+  $('.guests-text').text($('.guests-slider-aside').slider('value') + ' People');
+
+  $('.long-slider').slider({
+    range: "min",
+    min: 1,
+    max: 5,
+    value: 2,
+    slide: function(event, ui) {
+      $('.long-text').text(ui.value + ' Hours');
+    }
+  });
+
+  $('.long-text').text($('.long-slider').slider('value') + ' Hours');
+
 
   var $bgs = $('.bgs');
 
