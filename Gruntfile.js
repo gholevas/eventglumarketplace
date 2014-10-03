fdb066247d8ffafd5820800478e90631d815db47
Mauricio Gamboa <magalocr@gmail.com>
2014-10-03 11:08:17 -0600

    Gallery

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,17 +54,9 @@ module.exports = function(grunt) {
     watch: {
       js: {
         files: [
-        'bower_components/jquery/dist/jquery.js',
-        'bower_components/jquery-ui/jquery-ui.min.js',
-        'bower_components/jquery-ui/ui/widget.js',
-        'bower_components/jquery-ui/ui/mouse.js',
-        'public/js/lib/jquery.ui.touch-punch.min.js',
-        'bower_components/bootstrap/dist/js/bootstrap.js',
-        'bower_components/angular/angular.js',
         'public/js/app.js',
         'public/js/directives.js',
-        'public/js/eventsGlue.js',
-        'public/js/main.js'
+        'public/js/eventsGlue.js'
         ],
         tasks: ['concat:js', 'uglify:js']
       },
